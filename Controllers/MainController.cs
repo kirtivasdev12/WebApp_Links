f738a3965ea018c4ea3228435e3fd9563e269f08
Kirti Vasdev <kirtivasdev12@gmail.com>
2020-07-03 01:10:59 -0400

    Add project files.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Links.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+
+        {
+
+            return View();
+
+        }
+
+
+
+        public ActionResult Contactus()
+
+        {
+
+            return View();
+
+        }
+
+       
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApp_Links.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
